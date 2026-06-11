dec1df06bbe3681f3d1b3203b01439288aea3a88
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-06-11 10:00:19 -0300

    adicionando repositories

---
+++ src/DevIO.Business/Interfaces/IFornecedorRepository.cs
@@ -0,0 +1,11 @@
+﻿using DevIO.Business.Models;
+
+namespace DevIO.Business.Interfaces;
+
+public interface IFornecedorRepository : IRepository<Fornecedor>
+{
+    Task<Fornecedor> ObterFornecedorEndereco(Guid id);    
+    Task<Fornecedor> ObterFornecedorProdutoEndereco(Guid id);
+    
+    Task<Endereco> ObterEderecoPorFornecedor(Guid fornecedorId);
+}
--- src/DevIO.Business/Models/Fornecedor.cs
+++ src/DevIO.Business/Models/Fornecedor.cs
@@ -6,6 +6,7 @@ public class Fornecedor : Entity
     public string? Documento { get; set; }
     public TipoFornecedor TipoFornecedor { get; set; }
     public bool Ativo { get; set; }
+    
     public Endereco? Endereco { get; set; }
 }
 
