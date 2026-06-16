886c6d19d21366c509152ad525a9c5171cd77938
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-06-16 12:23:13 -0300

    adicionando referencia de Data e Business

---
+++ src/DevIO.Data/Context/MeuDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.Data.Context;
+
+public class MeuDbContext : DbContext
+{
+    public MeuDbContext(DbContextOptions<MeuDbContext> options) 
+        : base(options) 
+    { }
+    
+    public DbSet<Produto> Produtos { get; set; }
+    public DbSet<Endereco> Enderecos { get; set; }
+    public DbSet<Fornecedor> Fornecedores { get; set; }
+}
