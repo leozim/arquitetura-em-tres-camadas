0f23b36e4d672e1c679be876757a15ad0da8b2ed
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-06-16 11:51:20 -0300

    implementando as notificacoes

---
+++ src/DevIO.Business/Notificacoes/Notificacao.cs
@@ -0,0 +1,11 @@
+﻿namespace DevIO.Business.Notificacoes;
+
+public class Notificacao
+{
+    public Notificacao(string mensagem)
+    {
+        Mensagem = mensagem;
+    }
+    
+    public string Mensagem { get; }
+}
